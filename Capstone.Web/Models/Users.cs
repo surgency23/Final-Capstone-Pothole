4441f0b623757e6463fbc94c50764df37ccceac4
alex.pappa.1@gmail.com <alex.pappa.1@gmail.com>
2018-04-09 14:48:22 -0400

    Users Model

---
+++ Capstone.Web/Models/Users.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Capstone.Web.Models
+{
+    public class Users
+    {
+        public string UserName;
+        public string Password;
+        public string FirstName;
+        public string LastName;
+        public int IsEmployee;
+
+
+
+    }
+}
