2bab4106cfd6a98303952e6f372b8e4439ba24a1
alex.pappa.1@gmail.com <alex.pappa.1@gmail.com>
2018-04-10 13:19:39 -0400

    please

---
+++ Capstone.Web.Tests/DAL/UserDALTest.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Transactions;
+using System.Data.SqlClient;
+using System.Configuration;
+using Capstone.Web.Models;
+using Capstone.Web.DAL;
+
+namespace Capstone.Web.Tests.DAL
+{
+    [TestClass()]
+    public class UserDALTest
+    {
+        private TransactionScope tran;
+        private string connectionString = @"Data Source =.\sqlexpress;Initial Catalog = Potholes; Integrated Security = True";
+        
+        private int userID;
+
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            tran = new TransactionScope();
+
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                SqlCommand cmd;
+
+                conn.Open();
+
+                cmd = new SqlCommand(@"
+                     INSERT INTO [dbo].[Users]
+                     ([Username]
+                     ,[Password]
+                     ,[FirstName]
+                     ,[LastName]
+                     ,[Is_Employee]
+                     ,[Email])
+                        VALUES
+                    ('testUser',
+                    'Password123',
+                    'FirstName',
+                    'LastName',
+                    1,
+                    'TestEmail@test.com')", conn);
+                userID = cmd.ExecuteNonQuery();
+
+            }
+        }
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            tran.Dispose();
+        }
+
+        [TestMethod]
+        public void FindingTestUser()
+        {
+            UserSQLDAL userDAL = new UserSQLDAL(connectionString);
+            Users user = new Users
+            {
+                Username = "testUser",
+                Password = "Password123",
+                FirstName = "Test",
+                LastName = "LastName",
+                Is_Employee = 1,
+                Email = "TestEmail@test.com"
+
+            };
+            Assert.AreEqual(true, userDAL.CreateUser(user));
+        }
+    }
+}
--- Capstone.Web/Models/Users.cs
+++ Capstone.Web/Models/Users.cs
@@ -7,17 +7,14 @@ namespace Capstone.Web.Models
 {
     public class Users
     {
-        private string Username { get; set; }
-        private string Password { get; set; }
-        private string FirstName { get; set; }
-        private string LastName { get; set; }
-        private bool Is_Employee { get; set; } //bit identifier
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Is_Employee { get; set; } //bit identifier
+        public string Email { get; set; }
 
-        public string username;
-        public string password;
-        public string firstName;
-        public string lastName;
-        public int is_Employee; // bit pulled from DATABASE
+
 
     }
 }
