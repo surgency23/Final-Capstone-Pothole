ef83239305a766af8798946c2c886fdd00730816
Shanks <dshanks0811@gmail.com>
2018-04-10 11:11:15 -0400

    GoogleMaps Location Services Library added for manual address entry

--- Capstone.Web/Scripts/main.js
+++ Capstone.Web/Scripts/main.js
@@ -2,7 +2,6 @@
 
 $(document).ready(function () {
     $("#reportBtn").on("click", function (getLocation) {
-
             if (navigator.geolocation) {
                 navigator.geolocation.getCurrentPosition(showPosition);
             } else {
@@ -15,7 +14,7 @@ $(document).ready(function () {
 
         var today = new Date();
         var dd = today.getDate();
-        var mm = today.getMonth() + 1; //January is 0!
+        var mm = today.getMonth() + 1;
 
         var yyyy = today.getFullYear();
         if (dd < 10) {
@@ -28,4 +27,27 @@ $(document).ready(function () {
             $("#DateReported").val(today);
     });
 
+    $("#reportBtnMan").on("click", function (event) {
+        var address = $("#manualAddressEntry").val();
+        var city = $("#manualCityEntry").val();
+        var state = $("#manualStateEntry").val();
+
+        addressData = {
+            Address = address.toString(),
+            City = city.toString(),
+            State = state.toString()
+        }
+
+        var gls = newGoogleLocationService();
+        var latlong = gls.GetLatLongFromAddress(address);
+        $("#Latitude").val(latLong.Latitude);
+        $("#Longitude").val(latLong.Latitude);
+
+        //try {
+        //    var latlong = gls.GetLatLongFromAddress(address);
+
+        //}
+        //catch (System.Net.WebException ex){
+        //System.Console.WriteLine("Google Maps API Error {0}", ex.Message); }
+    });
 });
