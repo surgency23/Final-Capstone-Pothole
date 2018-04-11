28189083bf9c40e44def7d307542288bf69b178c
Shanks <dshanks0811@gmail.com>
2018-04-11 10:47:47 -0400

    Manual Address Entry and Coordnite Acquisition Completed

--- Capstone.Web/Models/AddressData2.cs
+++ Capstone.Web/Models/AddressData2.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Globalization;
+using System.Web.Mvc;
 
 namespace Capstone.Web.Models
 {
@@ -12,5 +14,33 @@ namespace Capstone.Web.Models
         public string State { get; set; }
         public string Zip { get; set; }
         public string Country { get; set; }
+        public int? Severity { get; set; }
+
+        public static List<SelectListItem> SeverityLevels
+        {
+            get
+            {
+                return new List<SelectListItem>()
+                {
+                    new SelectListItem { Text = "Minor", Value = "1" },
+                    new SelectListItem { Text = "Moderate", Value = "2" },
+                    new SelectListItem { Text = "Hazard", Value = "3" },
+                    new SelectListItem { Text = "Severe", Value = "4" },
+                    new SelectListItem { Text = "Extreme Danger", Value = "5" },
+                };
+            }
+        }
+
+        public override string ToString()
+        {
+            return String.Format(
+                CultureInfo.InvariantCulture,
+                "{0}{1}{2}{3}{4}",
+                Address != null ? Address + ", " : "",
+                City != null ? City + ", " : "",
+                State != null ? State + ", " : "",
+                Zip != null ? Zip + ", " : "",
+                Country != null ? Country : "").TrimEnd(' ', ',');
+        }
     }
 }
--- Capstone.Web/Models/Pothole.cs
+++ Capstone.Web/Models/Pothole.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
 
 namespace Capstone.Web.Models
 {
@@ -14,5 +16,19 @@ namespace Capstone.Web.Models
         public decimal Longitude { get; set; }
         public decimal Latitude { get; set; }
 
+        public static List<SelectListItem> SeverityLevels
+        {
+            get
+            {
+                return new List<SelectListItem>()
+                {
+                    new SelectListItem { Text = "Minor", Value = "1" },
+                    new SelectListItem { Text = "Moderate", Value = "2" },
+                    new SelectListItem { Text = "Hazard", Value = "3" },
+                    new SelectListItem { Text = "Severe", Value = "4" },
+                    new SelectListItem { Text = "Extreme Danger", Value = "5" },
+                };
+            }
+        }
     }
 }
