0ba89c47937fe022261ad57448fa2ab0c8241b79
Shanks <dshanks0811@gmail.com>
2018-04-11 09:09:31 -0400

    cleanup

--- Capstone.Web/Models/AddressData2.cs
+++ Capstone.Web/Models/AddressData2.cs
@@ -7,5 +7,10 @@ namespace Capstone.Web.Models
 {
     public class AddressData2
     {
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Zip { get; set; }
+        public string Country { get; set; }
     }
 }
