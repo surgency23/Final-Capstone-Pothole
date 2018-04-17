e9050683741ded824b281de4ad14dea1d027e587
Shanks <dshanks0811@gmail.com>
2018-04-17 10:11:27 -0400

    ClaimSubmit View

--- Capstone.Web/Models/DamageClaimModel.cs
+++ Capstone.Web/Models/DamageClaimModel.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
 
 namespace Capstone.Web.Models
 {
@@ -13,5 +15,22 @@ namespace Capstone.Web.Models
         public DateTime SubmissionDate { get; set; }
         public string Status { get; set; }
         public int Pothole_ID { get; set; }
+        public int Claim_ID { get; set; }
+
+        public static List<SelectListItem> StatusUpdate
+        {
+            get
+            {
+                return new List<SelectListItem>()
+                {
+                    new SelectListItem { Text = "Submitted", Value = "Submitted" },
+                    new SelectListItem { Text = "Reviewed", Value = "Reviewed" },
+                    new SelectListItem { Text = "Approved", Value = "Approved" },
+                    new SelectListItem { Text = "Denied", Value = "Denied" },
+                    new SelectListItem { Text = "Completed", Value = "Completed" },
+                };
+            }
+        }
     }
+
 }
