b8128c500c1cb45a01ba592ebd2bac0abecb44a4
Shanks <dshanks0811@gmail.com>
2018-04-19 09:42:00 -0400

    added missing file

---
+++ Capstone.Web/Scripts/main.js
@@ -0,0 +1,27 @@
+﻿﻿/// <reference path="../Scripts/jquery-3.1.1.js" />
+
+$(document).ready(function () {
+
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    } else {
+        x.innerHTML = "Geolocation is not supported by this browser.";
+    }
+    function showPosition(position) {
+        $("#Latitude").val(position.coords.latitude);
+        $("#Longitude").val(position.coords.longitude);
+    }
+
+    $("#manualButton").click(function () {
+        $("#content").load("/Home/ManualPotHoleEntry").$("body");
+    });
+
+    ﻿function ColorChange() {/*method for navbar color change*/
+        var x = document.getElementById("myTopnav");
+        if (x.className === "topnav") {
+            x.className += " responsive";
+        } else {
+            x.className = "topnav";
+        }
+    }
+});
